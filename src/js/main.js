0ab2b694be246bc97180dcde1c6b3f4c50d8eec6
Maksymenko Daryna <maksimenkodaryna@gmail.com>
2021-12-29 19:48:14 +0200

    Swiper

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,19 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader)
 
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true
+    },
+    breakpoints: {
+        992: {
+            spaceBetween: 80,
+        },
+    },
+});
